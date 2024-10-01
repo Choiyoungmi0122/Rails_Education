382fd73e2ff74f0891d9a6692e9c2290dd5b87b8
Choiyoungmi0122 <choiyoungmi0122@gmail.com>
2024-10-01 07:06:25 +0000

    first test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 	resources :comments
 	resources :articles
 	resources :tests
+	
+	
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	root 'class_lists#index'
 end
